b1e57bc415190657afac4d97d4bf94853f46e857
AkramAhmed <sarahsamya83@gmail.com>
2025-11-24 22:39:50 +0530

    second commit

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,22 @@
+const accountID = 12234;
+let accountEmail = "aa@aa.com";
+var accountPassword = "12323233";
+accountCity = "Noida";
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+console.log(typeof accountEmail);
+
+const person = {
+  name: "Akram",
+  age: 30,
+};
+
+person.name = "Danish";
+person.age = 77;
+console.log(person);
+
+const number = [1, 2, 3];
+number.push(5);
+
+console.log(number);
+console.log(number);
