c54adddcc34677cd0f2b332bf1d99b7af436b7a9
AkramAhmed <sarahsamya83@gmail.com>
2025-11-24 23:21:32 +0530

    third commit

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,8 +1,27 @@
 let score = "33";
-console.log(typeof score);
-console.log(typeof score);
+console.log(typeof score); //string
+console.log(score); //33
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(typeof valueInNumber);
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber); //33
+// ------------------------------------
+let runs = "33abc";
+console.log(typeof runs); //string
+console.log(runs); //33abc
+
+let valInNum = Number(runs);
+
+console.log(typeof valInNum); //number
+console.log(valInNum); //Nan
+
+// ------------------------------
+
+let value = null;
+console.log(typeof value); //object
+console.log(value); //null
+
+let number = Number(value);
+console.log(typeof number); // number
+console.log(number); // 0
