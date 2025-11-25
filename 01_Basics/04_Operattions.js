4999dca2392dcc388644e53866f3fd39cde9355b
AkramAhmed <sarahsamya83@gmail.com>
2025-11-25 15:14:28 +0530

    third commit

---
+++ 01_Basics/04_Operattions.js
@@ -0,0 +1,16 @@
+let a = "Hello ";
+let b = "Akram";
+
+let output = a + b;
+console.log(output);
+
+let c = 1 + "2"; //Concatination converts to string
+console.log(typeof c);
+
+let d = "1" + 2;
+console.log(typeof d);
+
+let e = "1" + 2 + 2;
+console.log(typeof e);
+
+console.log(1 + true); //converted to string
