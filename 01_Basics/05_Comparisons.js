006f9238c1858d03881b73749c519c9bd98ed795
AkramAhmed <sarahsamya83@gmail.com>
2025-11-25 20:35:13 +0530

    fourth commit

---
+++ 01_Basics/05_Comparisons.js
@@ -0,0 +1,19 @@
+// console.log("2" > 1); //True
+
+// console.log("2a" > 1); //false as 2a is converted to NaN
+
+// console.log("" >= 0); // true as empty string is converted to 0
+
+// console.log("1" == 1); //true
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+
+// console.log(null >= 0);
+
+console.log(undefined > 0);
+
+console.log(undefined == 0);
+
+console.log(undefined >= 0);
