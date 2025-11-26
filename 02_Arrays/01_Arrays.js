fc03fd9aada6009c0c429deee79b972ece0ea46f
AkramAhmed <sarahsamya83@gmail.com>
2025-11-26 23:23:33 +0530

    javascript

--- 02_Arrays/01_Arrays.js
+++ 02_Arrays/01_Arrays.js
@@ -1,4 +1,4 @@
-// const arr = [1, 23, "Akram", true];
+const arr = [1, 2, 3, 4, 5, 6];
 
 // console.log(arr);
 
@@ -9,3 +9,29 @@
 // console.log(arr.unshift("Ladakh"));
 
 // console.log(arr);
+
+// console.log(arr.push(99));
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.unshift(0));
+// console.log(arr);
+
+// console.log(arr.includes("Akram"));
+// console.log(arr.indexOf(true));
+
+// const array = arr.join();
+
+// console.log(array);
+
+console.log("A -> " + arr);
+const myn1 = arr.slice(0, 3);
+console.log(myn1 + "\n");
+
+console.log("B -> " + arr);
+const myn2 = arr.splice(0, 3);
+console.log(myn2);
+
+console.log("C -> " + arr);
